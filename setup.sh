1573e3e3ab139e43922f2e1236acb5e7850214f5
rifqi2708 <rifqiab2708@gmail.com>
2026-03-20 10:47:12 +0000

    dissable visualisation and increase sampling points

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
-# pip install gdown && \
-#     gdown 1LH9E5D273kOJXrUmBv_s2hXuOZV-dR65 -O weights.zip && \
-#     unzip weights.zip && \
-#     mv Self-supervised_Anatomical_Embeddings/checkpoints . && \
-#     mv Self-supervised_Anatomical_Embeddings/data . && \
-#     rm -r Self-supervised_Anatomical_Embeddings weights.zip 
+ pip install gdown && \
+    gdown 1LH9E5D273kOJXrUmBv_s2hXuOZV-dR65 -O weights.zip && \
+    unzip weights.zip && \
+    mv Self-supervised_Anatomical_Embeddings/checkpoints . && \
+    mv Self-supervised_Anatomical_Embeddings/data . && \
+    rm -r Self-supervised_Anatomical_Embeddings weights.zip 
 
-# pip install ipython
+pip install ipython
 
-# git branch dev origin/dev
-# git checkout dev
-# ipython misc/lymphnode_preprocess_crop_multi_process.py
+git branch dev origin/dev
+git checkout dev
+ipython misc/lymphnode_preprocess_crop_multi_process.py
 
-# git config --global user.email "rifqiab2708@gmail.com"
-# git config --global user.name "rifqi2708"
+git config --global user.email "rifqiab2708@gmail.com"
+git config --global user.name "rifqi2708"
 
 #input masks
 gdown --id 1fBLv7I8jg8yvAnkdKREBbESV6Vrx9833 -O masks.zip && \
