0ab7e6b65e4e03ea7fdca59f6286078c7674584c
rifqi2708 <rifqiab2708@gmail.com>
2026-04-03 15:17:31 +0000

    update setup

--- setup.sh
+++ setup.sh
@@ -7,24 +7,24 @@ pip install gdown && \
 
 pip install ipython
 
-git branch dev origin/dev
-git checkout dev
+# git branch dev origin/dev
+# git checkout dev
 ipython misc/lymphnode_preprocess_crop_multi_process.py
 
 git config --global user.email "rifqiab2708@gmail.com"
 git config --global user.name "rifqi2708"
 
 
-#quadra dataset males
-gdown 1R51bptSQLkhziDPzAAagv0KvVGGspcSF -O quadra_dataset_males.zip && \
-unzip quadra_dataset_males.zip && \
-rm quadra_dataset_males.zip && \
-mv quadra_dataset_males data
+# #quadra dataset males
+# gdown 1R51bptSQLkhziDPzAAagv0KvVGGspcSF -O quadra_dataset_males.zip && \
+# unzip quadra_dataset_males.zip && \
+# rm quadra_dataset_males.zip && \
+# mv quadra_dataset_males data
 
-#quadra dataset females
-gdown 1hR-Df0pt9FUwd_i4IOsyFYwohsehnJyX -O quadra_dataset_females.zip && \
-unzip quadra_dataset_females.zip && \
-rm quadra_dataset_females.zip && \
-mv quadra_dataset_females data
+# #quadra dataset females
+# gdown 1hR-Df0pt9FUwd_i4IOsyFYwohsehnJyX -O quadra_dataset_females.zip && \
+# unzip quadra_dataset_females.zip && \
+# rm quadra_dataset_females.zip && \
+# mv quadra_dataset_females data
 
 
